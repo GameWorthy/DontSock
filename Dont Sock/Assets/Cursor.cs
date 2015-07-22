b23867dedbd687016bd219b06bd04fbc31f784b9
kaue-rosa <kaue.aftimus@gmail.com>
2015-07-21 22:43:27 -0700

    making socks draggable

--- Dont Sock/Assets/Cursor.cs
+++ Dont Sock/Assets/Cursor.cs
@@ -3,15 +3,48 @@ using System.Collections;
 
 public class Cursor : MonoBehaviour {
 
-	// Use this for initialization
+	private Sock connectedSock = null;
+	private DistanceJoint2D joint;
+
+
 	void Start () {
-	
+		joint = GetComponent<DistanceJoint2D> ();
 	}
 	
-	// Update is called once per frame
 	void Update () {
+
+		if (Input.GetMouseButtonDown (0) ||
+		    Input.touchCount > 0 ) {
+			TryFindSock();
+		}
+
+		if (!connectedSock)
+			return;
+
+		if (Input.GetMouseButtonUp (0) ||
+			Input.touchCount > 0 && Input.GetTouch(0).phase == TouchPhase.Ended) {
+			connectedSock.Off ();
+			connectedSock = null;
+		}
+
+
 		Vector3 cursorPosition = Input.mousePosition;
 		cursorPosition = Camera.main.ScreenToWorldPoint (cursorPosition);
 		transform.position = cursorPosition;
+	}
+
+	void TryFindSock() {
+
+		Vector3 mousePosition = Camera.main.ScreenToWorldPoint (Input.mousePosition);
+
+		RaycastHit2D hit = Physics2D.Raycast(mousePosition, Vector2.zero);
+		if(hit.collider != null) {
+			connectedSock = hit.collider.gameObject.GetComponent<Sock>();
+			Vector3 distance = mousePosition - connectedSock.transform.position;
+			joint.connectedBody = connectedSock.Body;
+			joint.connectedAnchor = distance;
+			joint.distance = 0.05f;
+			connectedSock.On ();
+		}
 	}
 }
---
+++ Dont Sock/Assets/Sock.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Sock : MonoBehaviour {
+
+	private Rigidbody2D body;
+	public Rigidbody2D Body
+	{
+		get { return body; }
+	}
+
+	void Start () {
+		this.body = GetComponent<Rigidbody2D> ();
+	}
+
+	public void On() {
+		body.gravityScale = 1;
+	}
+
+	public void Off() {
+		body.gravityScale = 0;
+	}
+}
