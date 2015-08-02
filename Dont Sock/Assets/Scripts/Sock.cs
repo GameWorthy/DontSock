e08c5520e1ef20d1eb9c44468dce15707a5dfe56
kaue-rosa <kaue.aftimus@gmail.com>
2015-08-01 19:25:36 -0700

    playing around with colors, organizing scripts into folders and creating SockReader.cs

--- Dont Sock/Assets/Scripts/Sock.cs
+++ Dont Sock/Assets/Scripts/Sock.cs
@@ -1,11 +1,33 @@
 ﻿using UnityEngine;
 using System.Collections;
+using System.Collections.Generic;
 
 public class Sock : MonoBehaviour {
 
 	static float orderInLayer = -0.0f;
+	static int sock_id = 0;
+
+	private static List<Color> colors = new List<Color> {
+		Color.white,
+		Color.blue,
+		Color.cyan,
+		Color.gray,
+		Color.green,
+		Color.magenta,
+		Color.red,
+		Color.yellow,
+		new Color(34,53,255)
+	};
 
 	private SpriteRenderer sprite;
+	private int id = 0;
+	public int ID
+	{
+		get { return id; }
+		private set { id = value; }
+	}
+
+
 	private Rigidbody2D body;
 	public Rigidbody2D Body
 	{
@@ -15,8 +37,11 @@ public class Sock : MonoBehaviour {
 	void Start () {
 		this.body = GetComponent<Rigidbody2D> ();
 		this.sprite = GetComponent<SpriteRenderer> ();
-		this.transform.localRotation = Quaternion.Euler (0,0,Random.Range(0,90));
+		this.transform.localRotation = Quaternion.Euler (0,0,Random.Range(0,360));
+		id = sock_id;
+		this.sprite.color = colors [id];
 		LayerUp ();
+		sock_id++;
 	}
 
 	public void On() {
---
+++ Dont Sock/Assets/Scripts/SockReader.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SockReader : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
